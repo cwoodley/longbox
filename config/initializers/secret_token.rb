e5c498f176061cf1d8312d8a78e08f6a01ff392b
Cale Woodley <cale@loadedcommunications.com.au>
2013-05-07 11:27:38 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comics::Application.config.secret_token = 'c914af434adca9528cb53b63b8877964f9d9c94564290245fc40fdf982191b5e154288cf10ead7b240a58aa241d75d2f17a54b6570156666f17ad175fc44d2b7'
