9cf8ec2478654c21e7a95487d5411dcafa005627
Cale Woodley <cale@loadedcommunications.com.au>
2013-05-23 16:41:19 +0800

    styling changes

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20130522020432) do
     t.string   "slug"
   end
 
-  add_index "series", ["slug"], :name => "index_series_on_slug", :unique => true
+  add_index "series", ["slug"], :name => "index_series_on_slug"
   add_index "series", ["title"], :name => "index_series_on_title", :unique => true
 
   create_table "users", :force => true do |t|
