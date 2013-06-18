53e17d21e834e833f3664ccaf6b5f16e990a5f63
Cale Woodley <cale@cobalt343.com>
2013-06-18 15:12:58 +0800

    button formatting, toggle for uploading Issue cover via url

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-
 	$('.best_in_place').best_in_place();
+
+	$('#remove_cover_url-toggle').click(function(){
+		$('#remove_cover_url-group').toggleClass('hidden');
+	});
 });
