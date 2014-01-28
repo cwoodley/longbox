7921c04e07593550876e52891d23b0e17c83ca2d
Cale Woodley <cale@loadedcommunications.com.au>
2014-01-28 10:18:18 +0800

    added logic for Volumes

--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -1,8 +1,15 @@
 class VolumesController < ApplicationController
+  before_filter :series_id
+  before_filter :authenticate_user!, except: [:index, :show]
+  
   # GET /volumes
   # GET /volumes.json
   def index
-    @volumes = Volume.all
+    if params[:id]
+      @volumes = Series.find(params[:id]).volumes
+    else
+      @volumes = Volume.all
+    end
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,7 +20,11 @@ class VolumesController < ApplicationController
   # GET /volumes/1
   # GET /volumes/1.json
   def show
-    @volume = Volume.find(params[:id])
+    @volume = Volume.find(:first, :conditions => {:series_id => @series_id, :volume_number => params[:id]})
+    @series = Series.find(params[:series_id])
+
+    drop_breadcrumb(@series.title, series_path(@volume.series.slug))
+    drop_breadcrumb(@volume.volume_number)
 
     respond_to do |format|
       format.html # show.html.erb
@@ -34,17 +45,18 @@ class VolumesController < ApplicationController
 
   # GET /volumes/1/edit
   def edit
-    @volume = Volume.find(params[:id])
+    @volume = volume.find(:first, :conditions => {:series_id => @series_id, :volume_number => params[:id]})
   end
 
   # POST /volumes
   # POST /volumes.json
   def create
     @volume = Volume.new(params[:volume])
+    @series = Series.where(:slug => params[:series_id])
 
     respond_to do |format|
       if @volume.save
-        format.html { redirect_to @volume, notice: 'Volume was successfully created.' }
+        format.html { redirect_to series_volume_path(@volume.series,@volume.volume_number), notice: 'volume was successfully created.' }
         format.json { render json: @volume, status: :created, location: @volume }
       else
         format.html { render action: "new" }
@@ -60,7 +72,7 @@ class VolumesController < ApplicationController
 
     respond_to do |format|
       if @volume.update_attributes(params[:volume])
-        format.html { redirect_to @volume, notice: 'Volume was successfully updated.' }
+        format.html { redirect_to series_volume_path(@volume.series,@volume.volume_number), notice: 'volume was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -76,8 +88,15 @@ class VolumesController < ApplicationController
     @volume.destroy
 
     respond_to do |format|
-      format.html { redirect_to volumes_url }
+      format.html { redirect_to series_path(@volume.series) }
       format.json { head :no_content }
     end
   end
+
+  private
+
+  def series_id
+    @series_id = Series.where(:slug => params[:series_id])
+  end
+
 end
--- app/models/volume.rb
+++ app/models/volume.rb
@@ -1,7 +1,12 @@
 class Volume < ActiveRecord::Base
-  attr_accessible :own, :series_id, :volume_number
+  attr_accessible :volume_number, :own, :series_id, :cover, :remote_cover_url
   belongs_to :series
   
   validates(:volume_number, presence: true)
   validates(:series_id, presence: true)
+
+  mount_uploader :cover, CoverUploader
+
+  scope :owned, :conditions => {:own => true}
+  scope :wanted, :conditions => {:own => false}
 end
